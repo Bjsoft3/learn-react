7cd9a94cc2dc6402c5c7a1d9732c72980c1efedf
bhagirath_rajpurohit <bjsoft3@gmail.com>
2025-02-05 07:49:13 +0530

    Created two project using React and React with Vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import Chai from './Chai'
+
+function App() {
+  const username = "DevBJ"
+
+  return (
+    <>
+      <h1>Learn React | Bhagirath</h1>
+      <span>Username is {username}</span>
+      <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+      <h1>Vite React | Dev BJ</h1>
+      <Chai/>
+    </>
+  )
+}
+
+export default App
